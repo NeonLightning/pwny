e3517350e769990ced270087e71800cfea842921
NeonLightning <neonlightning@gmail.com>
2023-11-12 22:33:39 -0500

    now won't hammer api

--- weather.py
+++ weather.py
@@ -23,6 +23,7 @@ class WeatherForecast(plugins.Plugin):
     def on_loaded(self):
         self.api_key = None
         self.location = None
+        self.timer = 12
         self.api_key = config['main']['plugins']['weather']['api_key']
         logging.info(f"Weather Forecast Plugin api.{self.api_key}")
         self.location = config['main']['plugins']['weather']['location']
@@ -33,17 +34,22 @@ class WeatherForecast(plugins.Plugin):
                                                                    position=(90, 85), label_font=fonts.Small, text_font=fonts.Small))
         ui.add_element('main', components.LabeledValue(color=view.BLACK, label='', value='',
                                                             position=(90, 100), label_font=fonts.Small, text_font=fonts.Small))
-    def on_ui_update(self, ui):
-        weather_url = f"https://api.openweathermap.org/data/2.5/weather?q={self.location}&units=metric&appid={self.api_key}"
-        try:
-            weather_response = requests.get(weather_url).json()
-            current_temp = weather_response['main']['feels_like']
-            description = weather_response['weather'][0]['description']
-            ui.set('feels', f"TEMP:{current_temp}°C")
-            ui.set('main', f"WTHR:{description}")
-        except:
-            ui.set('main', f"WTHR:NA")
-            ui.set('feels', "Temp:NA")
+        
+    def on_internet_available(self, ui):
+        if self.timer == 12:
+            weather_url = f"https://api.openweathermap.org/data/2.5/weather?q={self.location}&units=metric&appid={self.api_key}"
+            try:
+                weather_response = requests.get(weather_url).json()
+                current_temp = weather_response['main']['feels_like']
+                description = weather_response['weather'][0]['description']
+                ui.set('feels', f"TEMP:{current_temp}°C")
+                ui.set('main', f"WTHR:{description}")
+            except:
+                ui.set('main', f"WTHR:NA")
+                ui.set('feels', "Temp:NA")
+            self.timer == 0
+        else:
+            self.timer += 1
 
     def on_unload(self, ui):
         with ui._lock:
