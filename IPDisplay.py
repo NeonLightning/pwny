68fc40637401b265125e6e1cf8cbbe6990b269a0
NeonLightning <neonlightning@gmail.com>
2024-01-01 16:13:54 -0500

    fixed ipdisplay and weather

--- IPDisplay.py
+++ IPDisplay.py
@@ -12,11 +12,10 @@ from pwnagotchi.ui.components import LabeledValue
 from pwnagotchi.ui.view import BLACK
 import pwnagotchi.ui.fonts as fonts
 import pwnagotchi.plugins as plugins
-from pwnagotchi import config
-import pwnagotchi
 import logging
 import subprocess
-import time
+import ipaddress
+
 
 class IPDisplay(plugins.Plugin):
     __author__ = 'NeonLightning'
@@ -24,39 +23,45 @@ class IPDisplay(plugins.Plugin):
     __license__ = 'GPL3'
     __description__ = 'Display IP addresses on the Pwnagotchi UI'
 
-    def on_loaded(self):
-        self.device_list = config['main']['plugins']['IPDisplay'].get('devices', [])
-        if self.device_list == None:
-            self.device_list = [bnep0]
+    def __init__(self):
+        self.options = dict()
+        self.device_list = []
         self.device_index = 0
-        self.ui_element_name = 'ip1'
-        self.ready = False  # Initialize ready state to False
-        logging.info("IP Display Plugin loaded.")
+        self.ready = False
 
-    def on_ready(self, agent):
+    def on_loaded(self):
+        self.device_list = self.options['devices']
+        if self.device_list is None:
+            self.device_list = ['bnep0']
+        self.device_index = 0
+        logging.debug("IP Display Plugin loaded.")
+        
+    def on_ready(self):
         self.ready = True
+        logging.debug("IP Display Plugin ready.")
 
     def on_ui_setup(self, ui):
         pos1 = (150, 13)
-        ui.add_element(self.ui_element_name, LabeledValue(color=BLACK, label="", value='Initializing...',
-                                            position=pos1, label_font=fonts.Small, text_font=fonts.Small))
+        ui.add_element('ip1', LabeledValue(color=BLACK, label="", value='Initializing...',
+                                           position=pos1, label_font=fonts.Small, text_font=fonts.Small))
 
     def on_ui_update(self, ui):
-        if not self.ready:
-            return
-        if not self.device_list:
-            logging.debug("No devices found in the list.")
-            return
         current_device = self.device_list[self.device_index]
+        logging.debug(f'IPDisplay Device {current_device}')
         command = f"ip -4 addr show {current_device} | awk '/inet / {{print $2}}' | cut -d '/' -f 1 | head -n 1"
-        netip = subprocess.getoutput(command)
-        if netip != '':
-            ui.set(self.ui_element_name, f'{current_device}:{netip}')
+        netip = subprocess.getoutput(command).strip()
+        logging.debug(f'IPDisplay IP {netip}')
+        if netip:
+            try:
+                ipaddress.ip_address(netip)
+                ui.set('ip1', f'{current_device}:{netip}')
+            except ValueError:
+                logging.debug(f"Invalid IP address found for {current_device}: {netip}")
         else:
             logging.debug(f"No IP address found for {current_device}")
         self.device_index = (self.device_index + 1) % len(self.device_list)
 
     def on_unload(self, ui):
         self.ready = False
-        ui.remove_element(self.ui_element_name)
-        logging.info("IP Display Plugin unloaded.")
+        ui.remove_element('ip1')
+        logging.debug("IP Display Plugin unloaded.")
--- weather.py
+++ weather.py
@@ -2,12 +2,10 @@
 # main.plugins.weather.enabled = true
 # main.plugins.weather.api_key = ""
 # https://home.openweathermap.org/api_keys
-# main.plugins.weather.areacode = "postal/zip"
+# main.plugins.weather.areacode = "postal or zip"
 # main.plugins.weather.countrycode = "countrycode"
-# main.plugins.weather.gps = "/dev/ttyACM0"
-# (even if you don't have a gps set this...)
+# main.plugins.weather.gpson = true or false
 # but if you want gps for weather you'll need gps.py or gps_more.py
-# REQUIRES CUSTOM-FACES-MOD for icon display
 
 import os, logging, re, pwnagotchi, toml, json, requests, urllib.request, shutil
 from pwnagotchi import plugins, config
@@ -17,13 +15,34 @@ import pwnagotchi.ui.fonts as fonts
 import pwnagotchi.plugins as plugins
 from pwnagotchi.ui.components import Text
 from pwnagotchi.bettercap import Client
-from PIL import ImageDraw
+from PIL import ImageDraw, ImageOps, Image
 
-agent = Client('localhost', port=8081, username="pwnagotchi", password="pwnagotchi");    
+agent = Client('localhost', port=8081, username="pwnagotchi", password="pwnagotchi");
+
+class WeatherIcon(pwnagotchi.ui.components.Widget):
+    def __init__(self, value="", position=(0, 0), color=0, png=False):
+        super().__init__(position, color)
+        self.value = value
+
+    def draw(self, canvas, drawer):
+        if self.value is not None:
+            self.image = Image.open(self.value)
+            self.image = self.image.convert('RGBA')
+            self.pixels = self.image.load()
+            for y in range(self.image.size[1]):
+                for x in range(self.image.size[0]):
+                    if self.pixels[x,y][3] < 255:    # check alpha
+                        self.pixels[x,y] = (255, 255, 255, 255)
+            if self.color == 255:
+                self._image = ImageOps.colorize(self.image.convert('L'), black = "white", white = "black")
+            else:
+                self._image = self.image
+            self.image = self._image.convert('1')
+            canvas.paste(self.image, self.xy)    
 
 class WeatherForecast(plugins.Plugin):
     __author__ = 'NeonLightning'
-    __version__ = '1.1.0'
+    __version__ = '1.4.0'
     __license__ = 'GPL3'
     __description__ = 'A plugin that displays the weather forecast on the pwnagotchi screen.'
     __name__ = 'WeatherForecast'
@@ -37,60 +56,65 @@ class WeatherForecast(plugins.Plugin):
         
     def on_loaded(self):
         self.previous_seticon = None
-        self.api_key = None
         self.areacode = None
         self.country = None
         self.lat = None
         self.lon = None
         self.cords = None
         self.weather_response = None
-        self.timer = 12
         self.plugin_dir = os.path.dirname(os.path.realpath(__file__))
         self.icon_path = os.path.join(self.plugin_dir, "weather", "display.png")
-        self.icon = Text(value=self.icon_path, png=True, position=(147, 35))
         self.api_key = config['main']['plugins']['weather']['api_key']
         self.areacode = config['main']['plugins']['weather']['areacode']
         self.country = config['main']['plugins']['weather']['countrycode']
-        self.gps = config['main']['plugins']['weather']['gps']
+        self.gpson = config['main']['plugins']['weather']['gpson']
         self.geo_url = f"http://api.openweathermap.org/geo/1.0/zip?zip={self.areacode},{self.country}&appid={self.api_key}"
-        self._update_lat_lon()
-        logging.info(f"Weather Forecast Loaded")
+
+    def on_ready(self, agent):
+        self.timer = 12
+        if self._is_internet_available():
+                self._update_lat_lon()
+                self.weather_response = requests.get(self.weather_url).json()
+                logging.info("Weather Ready")
 
     def _update_lat_lon(self):
-        if config['main']['plugins']['gps']['enabled'] or config['main']['plugins']['gps_more']['enabled']:
-            try:
-                info = agent.session()
-                coords = info.get("gps", {})
-                if all([coords.get("Latitude"), coords.get("Longitude")]):
-                    self.lat = coords["Latitude"]
-                    self.lon = coords["Longitude"]
-            except Exception as err:
-                logging.warn(f"Failed to get GPS coordinates: {err}")
+        if self.gpson == True:
+            if config['main']['plugins']['gps']['enabled'] or config['main']['plugins']['gps_more']['enabled']:
+                try:
+                    info = agent.session()
+                    coords = info.get("gps", {})
+                    if all([coords.get("Latitude"), coords.get("Longitude")]):
+                        self.lat = coords["Latitude"]
+                        self.lon = coords["Longitude"]
+                except Exception as err:
+                    logging.warn(f"Failed to get GPS coordinates: {err}")
+            else:
+                pass
         else:
+            logging.debug("weather update location gps disabled")
             try:
                 geo_response = requests.get(self.geo_url).json()
                 self.lat = geo_response.get('lat')
                 self.lon = geo_response.get('lon')
             except Exception as err:
-                logging.error(f"Error fetching latitude and longitude: {err}")
+                logging.error(f"Error fetching latitude:{self.lat} and longitude:{self.lon} error:{err}")
         self.weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={self.lat}&lon={self.lon}&appid={self.api_key}"
+        logging.debug (f"weather url: {self.weather_url}")
         if self._is_internet_available():
             self.weather_response = requests.get(self.weather_url).json()
 
     def on_ui_setup(self, ui):
-        logging.info("Weather setup")
-        ui.add_element('icon', self.icon)        
-        logging.info("Weather setup icon")
+        ui.add_element('icon', WeatherIcon(value=self.icon_path, png=True, position=(147, 35)))        
         ui.add_element('feels', components.LabeledValue(color=view.BLACK, label='', value='',
                                                                    position=(90, 85), label_font=fonts.Small, text_font=fonts.Small))
-        logging.info("Weather setup feelslike")
         ui.add_element('main', components.LabeledValue(color=view.BLACK, label='', value='',
                                                             position=(90, 100), label_font=fonts.Small, text_font=fonts.Small))
-        logging.info("Weather setup main")
+        self._update_lat_lon()
+        logging.debug("Weather ui set")
     
     def on_epoch(self, agent, epoch, epoch_data):
         if self._is_internet_available():
-            if self.timer == 12:
+            if self.timer >= 12:
                 self.weather_response = requests.get(self.weather_url).json()
                 self.timer = 0
                 logging.info("Weather Updated")
@@ -98,27 +122,27 @@ class WeatherForecast(plugins.Plugin):
                 self.timer += 1
 
     def on_ui_update(self, ui):
-        try:
-            tempk = self.weather_response['main']['feels_like']
-            tempc = round(tempk - 273.15, 1)
-            description = self.weather_response['weather'][0]['main']
-            seticon = self.weather_response['weather'][0]['icon']
-            source_path = os.path.join(self.plugin_dir, "weather", f"{seticon}.png")
-            if config['ui']['faces']['png']:
+        if self.weather_response:
+            try:
+                tempk = self.weather_response['main']['feels_like']
+                tempc = round(tempk - 273.15, 1)
+                description = self.weather_response['weather'][0]['main']
+                seticon = self.weather_response['weather'][0]['icon']
+                source_path = os.path.join(self.plugin_dir, "weather", f"{seticon}.png")
                 if seticon != self.previous_seticon:
                     if os.path.exists(source_path):
-                        logging.info(f"Copying icon from {source_path}")
+                        logging.debug(f"Weather Copying icon from {source_path}")
                         shutil.copy(source_path, os.path.join(self.plugin_dir, "weather", "display.png"))
                     else:
                         ui.set('main', 'WTHR: Icon Not Found')
-                        logging.info(f"Weather ERROR: ICON NOT FOUND {source_path}")
+                        logging.error(f"Weather ERROR: ICON NOT FOUND {source_path}")
                     self.previous_seticon = seticon
-            ui.set('feels', f"TEMP:{tempc}°C")
-            ui.set('main', f"WTHR:{description}")
-        except Exception as e:
-            ui.set('main', 'WTHR: Error')
-            ui.set('feels', f'Temp: {e}')
-            logging.info(f"Weather ERROR: {e}")
+                ui.set('feels', f"TEMP:{tempc}°C")
+                ui.set('main', f"WTHR:{description}")
+            except Exception as e:
+                ui.set('main', 'WTHR: Error')
+                ui.set('feels', f'Temp: {e}')
+                logging.exception(f"Weather ERROR: {e}")
 
     def on_unload(self, ui):
         with ui._lock:
@@ -130,11 +154,10 @@ class WeatherForecast(plugins.Plugin):
                 ui.remove_element('main')
             except KeyError:
                 pass
-            if config['ui']['faces']['png']:
-                try:
-                    ui.remove_element('icon')
-                    self.icon_path = os.path.join(self.plugin_dir, "weather", "circle.png")
-                    shutil.copy(self.icon_path, os.path.join(self.plugin_dir, "weather", "display.png"))
-                except KeyError:
-                    pass
+            try:
+                ui.remove_element('icon')
+                self.icon_path = os.path.join(self.plugin_dir, "weather", "circle.png")
+                shutil.copy(self.icon_path, os.path.join(self.plugin_dir, "weather", "display.png"))
+            except KeyError:
+                pass
             logging.info("Weather Plugin unloaded")
