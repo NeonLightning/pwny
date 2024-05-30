790093453b8fa825f84e7500869f3c384df3eaa3
NeonLightning <neonlightning@gmail.com>
2024-05-30 16:22:29 -0400

    Update fluxmod.py

--- fluxmod.py
+++ fluxmod.py
@@ -1,26 +1,23 @@
 # set these to 24hr time values
 # main.plugins.fluxmod.invert_on_time = "20:00"
 # main.plugins.fluxmod.invert_off_time = "6:00"
-
 import toml, os, threading, logging, subprocess
 from datetime import datetime, time
 from pwnagotchi import plugins
 
 class Fluxmod(plugins.Plugin):
     __author__ = 'NeonLightning'
-    __version__ = '1.0.0'
+    __version__ = '1.0.3'
     __license__ = 'GPL3'
     __description__ = 'Changes ui.invert on a timer'
 
     def on_loaded(self):
         self.config_path = '/etc/pwnagotchi/config.toml'
-        self.invert_on_time = None
-        self.invert_off_time = None
         self.stop_event = threading.Event()
-        logging.info('[fluxmod] Fluxmod plugin loaded')
         self.load_config()
         self.thread = threading.Thread(target=self.run)
         self.thread.start()
+        logging.info('[fluxmod] Fluxmod plugin loaded')
 
     def on_unload(self, _):
         self.stop_event.set()
@@ -48,25 +45,41 @@ class Fluxmod(plugins.Plugin):
     def update_invert_ui(self):
         try:
             now = datetime.now().time()
-            if self.invert_on_time <= self.invert_off_time:
-                invert = self.invert_on_time <= now <= self.invert_off_time
-            else:
-                invert = now >= self.invert_on_time or now <= self.invert_off_time
-            logging.debug(f'[fluxmod] Inversion status: {invert}')
-            config_file = '/etc/pwnagotchi/config.toml'
-            with open(config_file, 'r') as f:
-                config_lines = f.readlines()
-            for i, line in enumerate(config_lines):
-                if 'ui.invert' in line:
-                    if str(invert).lower() not in line.lower():
-                        config_lines[i] = f'ui.invert = {str(invert).lower()}\n'
-                        with open(config_file, 'w') as f:
-                            f.writelines(config_lines)
-                        os.system('sudo systemctl restart pwnagotchi')
-                        logging.info(f'[fluxmod] Updated ui.invert to {invert}')
-                    break
+            invert = self.should_invert(now)
+            if self.update_config(invert):
+                os.system('sudo systemctl restart pwnagotchi')
+                logging.info(f'[fluxmod] Updated ui.invert to {invert}')
         except Exception as e:
             logging.error(f'[fluxmod] Error updating ui.invert: {e}')
+
+    def should_invert(self, current_time):
+        if self.invert_on_time <= self.invert_off_time:
+            return self.invert_on_time <= current_time <= self.invert_off_time
+        return current_time >= self.invert_on_time or current_time <= self.invert_off_time
+
+    def update_config(self, invert):
+        try:
+            with open(self.config_path, 'r') as f:
+                config_lines = f.readlines()
+            current_invert = None
+            new_invert = 'true' if invert else 'false'
+            for i, line in enumerate(config_lines):
+                if 'ui.invert' in line:
+                    if 'true' in line.lower():
+                        current_invert = 'true'
+                    elif 'false' in line.lower():
+                        current_invert = 'false'
+                    if current_invert != new_invert:
+                        config_lines[i] = f'ui.invert = {new_invert}\n'
+                    break
+            if current_invert != new_invert:
+                with open(self.config_path, 'w') as f:
+                    f.writelines(config_lines)
+                return True
+            return False
+        except Exception as e:
+            logging.error(f'[fluxmod] Error reading/writing config file: {e}')
+            return False
 
 if __name__ == "__main__":
     config_file = '/etc/pwnagotchi/config.toml'
