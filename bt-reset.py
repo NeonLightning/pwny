cebf59fa5726d5f021509debf5cb25b891170b6b
NeonLightning <neonlightning@gmail.com>
2024-05-30 13:45:04 -0400

    Update bt-reset.py
    now displays restart timeout

--- bt-reset.py
+++ bt-reset.py
@@ -17,21 +17,32 @@ class BTReset(Plugin):
         self.selfrunning = False
         logging.info("[BT-Reset] plugin unloaded.")
 
+    def on_ui_update(self, ui):
+        tether_status = ui.get("bluetooth")
+        if tether_status is not None:
+            ui.set("bluetooth", tether_status + " *")
+        if self.remaining_timeout is not None:
+            remaining_seconds = int(self.remaining_timeout)
+            logging.debug(f"Remaining seconds: {remaining_seconds}")
+            ui.set("bluetooth", tether_status + " * " + str(remaining_seconds) + "s")
+
     def check_bluetooth_status(self):
         result = subprocess.run(['bluetoothctl', 'info'], capture_output=True, text=True)
         if 'Connected: yes' in result.stdout:
             self.last_connected = datetime.now()
             if not self.was_connected:
-                logging.info(f"[BT-Reset] Bluetooth is connected. Updating last connected time.")
+                logging.info("[BT-Reset] Bluetooth is connected. Updating last connected time.")
                 self.was_connected = True
         else:
-            time_disconnected = datetime.now() - self.last_connected 
-            remaining_timeout = self.timeout_minutes - time_disconnected
-            logging.info(f"[BT-Reset] No active Bluetooth connections detected. Disconnected for {time_disconnected}. Restarting Pwnagotchi service in {remaining_timeout} minutes.")
+            logging.debug("[BT-Reset] Bluetooth is not connected")
+            time_disconnected = datetime.now() - self.last_connected
+            logging.debug(f"[BT-Reset] Bluetooth time disconnected. {time_disconnected}")
+            self.remaining_timeout = self.timeout_minutes * 60 - time_disconnected.total_seconds()
+            logging.info(f"[BT-Reset] No active Bluetooth connections detected. Disconnected for {time_disconnected}. Restarting Pwnagotchi service in {self.remaining_timeout} seconds.")
             if self.was_connected:
-                logging.info(f"[BT-Reset] Bluetooth has been disconnected.")
+                logging.info("[BT-Reset] Bluetooth has been disconnected.")
                 self.was_connected = False
-            if datetime.now() - self.last_connected > timedelta(minutes=self.timeout_minutes):
+            if self.remaining_timeout <= 0:
                 logging.info(f"[BT-Reset] Bluetooth not connected for {self.timeout_minutes} minutes. Restarting Pwnagotchi service.")
                 subprocess.run(['sudo', 'systemctl', 'restart', 'pwnagotchi'], check=True)
 
@@ -43,4 +54,4 @@ class BTReset(Plugin):
         logging.info(f"[BT-Reset] plugin loaded with timeout of {self.timeout_minutes} minutes.")
         while self.selfrunning:
             self.check_bluetooth_status()
-            time.sleep(60)
+            time.sleep(10)
