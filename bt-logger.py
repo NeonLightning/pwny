eddd4d1064ab19075d039c9210630c42744bb5a7
NeonLightning <neonlightning@gmail.com>
2024-06-08 10:30:06 -0400

    get bt-logger working

--- bt-logger.py
+++ bt-logger.py
@@ -1,4 +1,4 @@
-import pwnagotchi, logging, re, subprocess, io
+import pwnagotchi, logging, re, subprocess, io, socket, json
 import pwnagotchi.plugins as plugins
 from pwnagotchi.ui.components import LabeledValue
 from pwnagotchi.ui.view import BLACK
@@ -11,6 +11,8 @@ class BTLog(plugins.Plugin):
     __description__ = 'Logs and displays a count of bluetooth devices seen.'
 
     def on_loaded(self):
+        self.gps = self.options.get('gps')
+        self.check_and_update_config('main.plugins.bt-logger.gps', 'false')
         self.count = 0
         self.interim_file = '/root/.btinterim.log'
         self.output = '/root/bluetooth.log'
@@ -20,6 +22,7 @@ class BTLog(plugins.Plugin):
                     self.count = sum(1 for line in log_file)
         except FileNotFoundError:
             with open(self.output, 'w'):
+                self.count = 0
                 pass
         logging.info('[BT-Log] Loaded')
         self.running = True
@@ -35,8 +38,11 @@ class BTLog(plugins.Plugin):
         logging.info('[BT-Log] Unloaded')
 
     def on_ui_setup(self, ui):
-        ui.add_element('bt-log', LabeledValue(color=BLACK, label='BT#:', value=str(self.count), position=(100, 100),
+        try:
+            ui.add_element('bt-log', LabeledValue(color=BLACK, label='BT#:', value='0', position=(0, 80),
                                         label_font=fonts.Small, text_font=fonts.Small))
+        except:
+            logging.error(f"[BT-Log] UI not made")
 
     def on_ui_update(self, ui):
         try:
@@ -44,17 +50,61 @@ class BTLog(plugins.Plugin):
                 if isinstance(log_file, io.TextIOBase):
                     self.count = sum(1 for line in log_file)
         except FileNotFoundError:
+            self.count = 0
             with open(self.output, 'w'):
                 pass
         ui.set('bt-log', str(self.count))
+        
+    def get_gps_coordinates(self):
+        try:
+            gpsd_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            gpsd_socket.connect(('localhost', 2947))
+            gpsd_socket.sendall(b'?WATCH={"enable":true,"json":true}')
+            while True:
+                data = gpsd_socket.recv(4096).decode('utf-8')
+                for line in data.splitlines():
+                    try:
+                        report = json.loads(line)
+                        if report['class'] == 'TPV' and 'lat' in report and 'lon' in report:
+                            return report['lat'], report['lon']
+                    except json.JSONDecodeError:
+                        continue
+        except Exception as e:
+            logging.error(f"[Weather2Pwn] Error getting GPS coordinates: {e}")
+            return None, None
 
+    def check_and_update_config(self, key, value):
+        config_file = '/etc/pwnagotchi/config.toml'
+        try:
+            with open(config_file, 'r') as f:
+                config_lines = f.readlines()
+            key_found = False
+            insert_index = -1
+            for i, line in enumerate(config_lines):
+                if 'main.plugins.bt-logger.enabled' in line:
+                    key_found = True
+                    insert_index = i + 1
+                    break
+            key_found = False
+            for line in config_lines:
+                if key in line:
+                    key_found = True
+                    break
+            if not key_found:
+                config_lines.insert(insert_index, f"{key} = {value}\n")
+                with open(config_file, 'w') as f:
+                    f.writelines(config_lines)
+                logging.info(f"[BT-Log] Added {key} to the config file with value {value}")
+        except Exception as e:
+            logging.error(f"[BT-Log] Exception occurred while processing config file: {e}")
+            
     def remove_ansi_escape_sequences(self, text):
         ansi_escape = re.compile(r'\x1B[@-_][0-?]*[ -/]*[@-~]|\x1B\[.*?[@-~]|\^A\^B')
         return ansi_escape.sub('', text)
 
     def log_bluetooth_scan(self, output_file, interim_file):
         device_pattern = re.compile(r'NEW.*Device ([0-9A-F:]{17}) (.+)')
-        with open(output_file, 'w') as log_file:
+        with open(output_file, 'a') as log_file:
             process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                                        stderr=subprocess.PIPE, universal_newlines=True)
             process.stdin.write('scan on\n')
@@ -68,7 +118,16 @@ class BTLog(plugins.Plugin):
                     entry = f"{mac_address} {device_name}"
                     if not self.is_duplicate(entry, interim_file):
                         self.count += 1
-                        log_entry = f"{entry}\n"
+                        log_entry = f"{entry}"
+                        latitude, longitude = self.get_gps_coordinates()
+                        logging.info(f"[BT-Log] {log_entry}")
+                        if self.gps == True:
+                            if self.get_gps_coordinates() != None:
+                                log_entry = f"{log_entry}: {latitude}, {longitude}\n"
+                            else:
+                                pass
+                        else:
+                            pass
                         log_file.write(log_entry)
                         log_file.flush()
                         with open(interim_file, 'a') as interim:
